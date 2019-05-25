192a9529a3afc121206546e2910784b2e91d3fd5
dbrotz <43593771+dbrotz@users.noreply.github.com>
2019-05-24 23:33:27 -0700

    Improve canvas rendering performance

--- canvas/fire.js
+++ canvas/fire.js
@@ -25,15 +25,37 @@ const h = 200;
 
 const paletteSize = 80;
 
+function hslToRgb(h, s, l) {
+  const c = (1 - Math.abs(2 * l - 1)) * s;
+  const hp = h / 60;
+  const x = c * (1 - Math.abs(hp % 2 - 1));
+  let r, g, b;
+  switch (Math.floor(hp)) {
+  case 0: [r, g, b] = [c, x, 0]; break;
+  case 1: [r, g, b] = [x, c, 0]; break;
+  case 2: [r, g, b] = [0, c, x]; break;
+  case 3: [r, g, b] = [0, x, c]; break;
+  case 4: [r, g, b] = [x, 0, c]; break;
+  case 5: [r, g, b] = [c, 0, x]; break;
+  default:
+    throw new Error('Invalid hue: ' + h);
+  }
+  const m = l - c / 2;
+  let color = [r, g, b].map(x => x + m);
+  color = color.map(x => Math.floor(x * 256));
+  color = color.map(x => Math.min(255, Math.max(0, x))); // clamp
+  return color;
+}
+
 function genPalette() {
   const palette = [];
 
   for (let i = 0; i < paletteSize; i++) {
     const t = i / (paletteSize - 1);
     const h = 60 * t;
-    const s = 100;
-    const l = 100 * t;
-    palette[i] = `hsl(${h}, ${s}%, ${l}%)`;
+    const s = 1;
+    const l = t;
+    palette[i] = hslToRgb(h, s, l);
   }
 
   return palette;
@@ -62,9 +84,12 @@ function fireStep(buffer) {
   }
 }
 
-function plotPixel(ctx, x, y, style) {
-  ctx.fillStyle = style;
-  ctx.fillRect(x, y, 1, 1);
+function initImageData(imageData) {
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    for (let j = 0; j < 3; j++)
+      imageData.data[i + j] = 0;
+    imageData.data[i + 3] = 255;
+  }
 }
 
 function main() {
@@ -73,9 +98,12 @@ function main() {
   canvas.width = w;
   canvas.height = h;
 
+  document.body.appendChild(canvas);
+
   const ctx = canvas.getContext('2d');
 
-  document.body.appendChild(canvas);
+  const imageData = ctx.createImageData(w, h);
+  initImageData(imageData);
 
   const palette = genPalette();
 
@@ -84,9 +112,13 @@ function main() {
 
   function render() {
     fireStep(buffer);
-    for (let y = 0; y < h; y++)
-      for (let x = 0 ; x < w; x++)
-        plotPixel(ctx, x, y, palette[buffer[y * w + x]]);
+    for (let i = 0; i < w * h; i++) {
+      const color = palette[buffer[i]];
+      const offset = i * 4;
+      for (let j = 0; j < 3; j++)
+          imageData.data[offset + j] = color[j];
+    }
+    ctx.putImageData(imageData, 0, 0);
     requestAnimationFrame(render);
   }
 
